5ea1a0f5f8e90fce5f736a2a61f7860db2e9944c
Елизавета Зуева <elizavetazueva@MacBook-Air-Elizaveta.local>
2023-07-27 15:02:51 +0300

    Add intent

---
+++ app/src/main/java/com/example/neobis_android_visitka/Doctors.kt
@@ -0,0 +1,6 @@
+package com.example.neobis_android_visitka
+
+import android.widget.ScrollView
+
+data class Doctors (val id: ScrollView, val name: String, val position: String) {
+}
--- app/src/main/java/com/example/neobis_android_visitka/MainActivity.kt
+++ app/src/main/java/com/example/neobis_android_visitka/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.neobis_android_visitka
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.neobis_android_visitka.databinding.ActivityMainBinding
@@ -7,9 +8,30 @@ import com.example.neobis_android_visitka.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+         val doctors = listOf(
+            Doctors(binding.NovikovCard, "Dr.Novikov", "Dentist"),
+            Doctors(binding.NurmatovCard, "Dr. Nurmatov", "Sanitarian"),
+            Doctors(binding.KhailovaCard, "Dr. Khailova", "Sanitarian"),
+            Doctors(binding.AlievaCard, "Dr. Alieva", "Surgeon"),
+            Doctors(binding.KolesovaCard, "Dr. Kolesova", "Orthodontist"),
+        )
+
+        doctors.forEach { item ->
+            item.id.setOnClickListener{
+                newIntent(item)
+            }
+        }
     }
+
+     fun newIntent(doctor: Doctors){
+        val intent = Intent(this, CardActivity::class.java)
+        startActivity(intent)
+    }
+
 }
